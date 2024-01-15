75b6cc560f50ec0aa633c0f08e55444e11de4850
AniketBaghel1601 <aniket357baghel@gmail.com>
2024-01-15 16:48:02 +0530

    done with crud part

--- backend/models/postsModel.js
+++ backend/models/postsModel.js
@@ -5,12 +5,12 @@ const postSchema = mongoose.Schema({
     body : {type : String, required : true},
     device : {type : String, enum : ["mobile", "tablet", "laptop"], required : true},
     no_of_comments : {type : Number, required : true},
-    userid : {type : String, required : true},
-    username : {type : String , required : true}
+    userId : {type : String, required : true},
+    username : {type : String, required : true}
 })
 
 
-const postModel = mongoose.connect('post',postSchema);
+const postModel = mongoose.model('post',postSchema);
 
 module.exports = {
     postModel
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -18,21 +18,45 @@ postRoute.get('/',auth,async(req,res)=>{
 })
 
 
-postRoute.post('/add',(req,res)=>{
+postRoute.post('/add',auth,async(req,res)=>{
     try {
         const payload = req.body;
         const post = new postModel(payload);
-        res.status(200).json('')
+       await post.save();
+        res.status(200).json({msg : "new post created"});
     } catch (error) {
-
+        res.status(200).json({err : error});
     }
 })
 
-postRoute.patch('/update',(req,res)=>{
+postRoute.patch('/update/:postId',auth,async(req,res)=>{
+    try {
+        
+    const postId = req.params.postId;
+    const post = await postModel.findOne({_id : postId});
+    if(post.userId===req.body.userId){
+       await postModel.findByIdAndUpdate({_id : postId},req.body);
+       res.status(200).json({msg : "post updated"});
+    }
+    else{
+        res.status(200).json({msg : 'enter the correct id !'});
+    }
 
+    } catch (error) {
+        console.log(error);
+    }
 })
 
-postRoute.delete('/delete',(req,res)=>{
+postRoute.delete('/delete/:postId',auth,async(req,res)=>{
+    const postId = req.params.postId;
+    const post = await postModel.findOne({_id : postId});
+    if(post.userId===req.body.userId){
+       await postModel.findByIdAndDelete({_id : postId},req.body);
+       res.status(200).json({msg : "post deleted"});
+    }
+    else{
+        res.status(200).json({msg : 'enter the correct id !'});
+    }
 
 })
 
